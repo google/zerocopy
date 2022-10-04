6dd0e8639924732cc986ed3ac80553ffc4ea4d76
Joshua Liebow-Feeser <joshlf@google.com>
2022-10-04 22:20:07 +0000

    Fix some copyright comment headers

--- generate-readme.sh
+++ generate-readme.sh
@@ -11,9 +11,9 @@ BODY=$(mktemp)
 DISCLAIMER_FOOTER=$(mktemp)
 
 cat > $COPYRIGHT_HEADER <<EOF
-# Copyright 2022 The Fuchsia Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+<!-- Copyright 2022 The Fuchsia Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file. -->
 
 EOF
 
