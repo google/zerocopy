bc3c9ac3091ee0dff2dea0620c7c0290e62065b0
Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
2022-11-05 16:53:45 -0700

    [WIP] Don't assume primitive type alignments (#118)
    Previously, we used the `u16` and `u64` types in a number of tests,
    relying on them having the alignments 2 and 8 respectively. While those
    types have those alignments on many platforms, those are not guaranteed
    to be their alignments on all platforms. In this commit, we replace
    most of these uses with types of the same size but guaranteed alignment.
    In a few places, we have to leave the uses as-is, but this isn't a big
    deal since our tests run in CI on platforms where the alignments are
    actually the values we're relying upon.
    
    Closes #116

---
+++ zerocopy-derive/tests/util.rs
@@ -0,0 +1,13 @@
+// Copyright 2022 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use zerocopy::AsBytes;
+
+// A `u16` with alignment 2.
+//
+// Though `u16` has alignment 2 on some platforms, it's not guaranteed.
+// By contrast, `AU16` is guaranteed to have alignment 2.
+#[derive(AsBytes, Copy, Clone)]
+#[repr(C, align(2))]
+pub struct AU16(u16);
