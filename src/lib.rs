bbab3e025b8b052767d0a0d05ceac7806ae26751
Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
2023-08-14 12:22:46 -0700

    Update TODO comment to link directly to Rust issue (#257)
    Doesn't actually resolve #2 in the sense of doing the task listed in the TODO, but closes it since we don't need a separate issue to track this.
    
    Closes #2

--- src/lib.rs
+++ src/lib.rs
@@ -347,8 +347,7 @@ pub unsafe trait FromZeroes {
         #[allow(clippy::as_conversions)]
         let max_alloc = (isize::MAX as usize).saturating_sub(align);
         assert!(size <= max_alloc);
-        // TODO(#2): Use `Layout::repeat` when `alloc_layout_extra` is
-        // stabilized.
+        // TODO(https://github.com/rust-lang/rust/issues/55724): Use `Layout::repeat` once it's stabilized.
         let layout =
             Layout::from_size_align(size, align).expect("total allocation size overflows `isize`");
 
