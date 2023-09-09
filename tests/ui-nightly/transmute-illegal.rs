c26336c8f611c72e4a692b819e367c3bda1b2a5c
Joshua Liebow-Feeser <hello@joshlf.com>
2023-09-09 03:00:45 -0700

    Add try_transmute!, try_transmute_{ref,mut}!
    TODO: Commit message body
    
    TODO:
    - Update try_transmute! docs to mention the size equality constraint?
      Add an example of this failing?
    - In `try_transmute!`, should the argument be dropped or forgotten (ie,
      `mem::forget`) when the transmute fails?
    
    Makes progress on #5

--- tests/ui-nightly/transmute-illegal.rs
+++ tests/ui-nightly/transmute-illegal.rs
@@ -8,3 +8,9 @@ fn main() {}
 
 // It is unsound to inspect the usize value of a pointer during const eval.
 const POINTER_VALUE: usize = zerocopy::transmute!(&0usize as *const usize);
+
+// `transmute!` and `try_transmute!` enforce size equality.
+const TOO_LARGE: u64 = zerocopy::transmute!(0u8);
+const TRY_TOO_LARGE: Option<u64> = zerocopy::try_transmute!(0u8);
+const TOO_SMALL: u8 = zerocopy::transmute!(0u64);
+const TRY_TOO_SMALL: Option<u8> = zerocopy::try_transmute!(0u64);
