bec380ccc698f2e2c5beaf1f525ae89fa1b7b8c6
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2024-02-12 15:05:48 +1100

    [ci] streamline check_fmt for performance

--- ci/check_fmt.sh
+++ ci/check_fmt.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -eo pipefail
-cargo fmt --check -p zerocopy
-cargo fmt --check -p zerocopy-derive
-rustfmt --check $(find tests -iname '*.rs' -type f)
-rustfmt --check $(find zerocopy-derive/tests -iname '*.rs' -type f)
-
+rustfmt --check $(find . -iname '*.rs' -type f -not -path './target/*')
