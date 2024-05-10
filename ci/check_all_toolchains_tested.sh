860f47f2072c04d997730e8cd39602b798247664
Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
2024-05-10 08:45:30 -0700

    Add missing copyright header comments (#1229)

--- ci/check_all_toolchains_tested.sh
+++ ci/check_all_toolchains_tested.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#
+# Copyright 2024 The Fuchsia Authors
+#
+# Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
+# <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
+# license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 set -eo pipefail
 
 # Check whether the set of toolchains tested in this file (other than
