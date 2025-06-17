dcf8f55ea08242ffb3cad1621ed3036525c26e55
Joshua Liebow-Feeser <hello@joshlf.com>
2025-06-17 15:45:22 -0400

    Fix parsing bug when --cfg doc_cfg is passed on aarch64
    Release 0.8.26.
    
    Closes #2594
    gherrit-pr-id: I66f25bd621bc867973bb5497b55c6101df52e7fe
    
    gherrit-pr-id: I4297087089c40574629ab26ae0f9e38e06c4c455

--- src/impls.rs
+++ src/impls.rs
@@ -1050,11 +1050,11 @@ mod simd {
             #[cfg(all(feature = "simd-nightly", target_arch = "powerpc64"))]
             powerpc64, powerpc64, vector_bool_long, vector_double, vector_signed_long, vector_unsigned_long
         );
+        #[cfg(zerocopy_aarch64_simd_1_59_0)]
         simd_arch_mod!(
             // NOTE(https://github.com/rust-lang/stdarch/issues/1484): NEON intrinsics are currently
             // broken on big-endian platforms.
             #[cfg(all(target_arch = "aarch64", target_endian = "little"))]
-            #[cfg(zerocopy_aarch64_simd_1_59_0)]
             #[cfg_attr(doc_cfg, doc(cfg(rust = "1.59.0")))]
             aarch64, aarch64, float32x2_t, float32x4_t, float64x1_t, float64x2_t, int8x8_t, int8x8x2_t,
             int8x8x3_t, int8x8x4_t, int8x16_t, int8x16x2_t, int8x16x3_t, int8x16x4_t, int16x4_t,
