08ba7b26b4be5119c757980b6f11203b19015a49
Briana Berger <brianaberger@google.com>
2025-11-07 14:23:40 -0800

    Optimize Ref<_, T> methods when T: Sized (#2752)
    Benchmarks are surprisingly showing minimal impact...
    
    With optimization:
    test r#ref::tests::bench_deref_mut_sized  ... bench:           0.72 ns/iter (+/- 0.09)
    test r#ref::tests::bench_deref_sized      ... bench:           0.70 ns/iter (+/- 0.09)
    test r#ref::tests::bench_from_bytes_sized ... bench:           5.27 ns/iter (+/- 0.69)
    test r#ref::tests::bench_into_mut_sized   ... bench:           0.70 ns/iter (+/- 0.07)
    test r#ref::tests::bench_into_ref_sized   ... bench:           0.94 ns/iter (+/- 0.08)
    
    Without optimization:
    test r#ref::tests::bench_deref_mut_sized  ... bench:           0.72 ns/iter (+/- 0.23)
    test r#ref::tests::bench_deref_sized      ... bench:           0.73 ns/iter (+/- 0.09)
    test r#ref::tests::bench_from_bytes_sized ... bench:           5.26 ns/iter (+/- 0.69)
    test r#ref::tests::bench_into_mut_sized   ... bench:           0.70 ns/iter (+/- 0.08)
    test r#ref::tests::bench_into_ref_sized   ... bench:           0.97 ns/iter (+/- 0.08)
    
    Closes #2752
    
    Co-authored-by: Joshua Liebow-Feeser <joshlf@google.com>

--- src/lib.rs
+++ src/lib.rs
@@ -334,12 +334,16 @@
     __ZEROCOPY_INTERNAL_USE_ONLY_NIGHTLY_FEATURES_IN_TESTS,
     feature(layout_for_ptr, coverage_attribute)
 )]
+#![cfg_attr(test, feature(test))]
 
 // This is a hack to allow zerocopy-derive derives to work in this crate. They
 // assume that zerocopy is linked as an extern crate, so they access items from
 // it as `zerocopy::Xxx`. This makes that still work.
 #[cfg(any(feature = "derive", test))]
 extern crate self as zerocopy;
+
+#[cfg(test)]
+extern crate test;
 
 #[doc(hidden)]
 #[macro_use]
