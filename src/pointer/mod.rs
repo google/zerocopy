f29130900f665866b8a5ffb43ae5788df0c062f3
Joshua Liebow-Feeser <joshlf@google.com>
2025-12-17 19:22:54 +0000

    [pointer] Add generic projection/cast framework
    Add `unsafe trait Project` and `unsafe trait Cast: Project`. `Cast` is
    implemented for any address-preserving cast, while `Project` generalizes
    to conversions which may not preserve the address of the referent (ie,
    field projections). Use these (mostly `Cast`, but some `Project`) to
    unify `PtrInner`/`Ptr` casts, field projections, and `SizeEq` casts.
    Replace a good amount of unsafe derive-generated code with uses of this
    machinery.
    
    Makes progress on #196
    Closes #2856
    
    gherrit-pr-id: Gdeb4f5a282b778a82175703218739a93074d0cc4

--- src/pointer/mod.rs
+++ src/pointer/mod.rs
@@ -38,3 +38,267 @@ where
 {
     ptr.as_bytes::<BecauseImmutable>().as_ref().iter().all(|&byte| byte == 0)
 }
+
+#[doc(hidden)]
+pub mod cast {
+    use core::{marker::PhantomData, mem, ptr::NonNull};
+
+    use crate::{
+        layout::{SizeInfo, TrailingSliceLayout},
+        HasField, KnownLayout,
+    };
+
+    /// A pointer cast or projection.
+    ///
+    /// # Safety
+    ///
+    /// `project` must be a provenance-preserving operation which preserves or
+    /// shrinks the set of referent bytes.
+    pub unsafe trait Project<Src: ?Sized, Dst: ?Sized> {
+        /// Projects a pointer from `Src` to `Dst`.
+        ///
+        /// # Safety
+        ///
+        /// `src` must be a non-null pointer which has provenance for its entire
+        /// referent. Its referent must be zero-sized or live in a single
+        /// allocation.
+        unsafe fn project(src: *mut Src) -> *mut Dst;
+    }
+
+    /// A [`Project`] which preserves the address of the referent – a pointer
+    /// cast.
+    ///
+    /// # Safety
+    ///
+    /// A `Cast` projection must preserve the address of the referent. It may
+    /// shrink the set of referent bytes, and it may change the referent's type.
+    pub unsafe trait Cast<Src: ?Sized, Dst: ?Sized>: Project<Src, Dst> {}
+
+    /// A no-op pointer cast.
+    #[derive(Default, Copy, Clone)]
+    #[allow(missing_debug_implementations)]
+    pub struct IdCast;
+
+    // SAFETY: `project` returns its argument unchanged, and so it is a
+    // provenance-preserving projection which preserves the set of referent
+    // bytes.
+    unsafe impl<T: ?Sized> Project<T, T> for IdCast {
+        #[inline(always)]
+        unsafe fn project(src: *mut T) -> *mut T {
+            src
+        }
+    }
+
+    // SAFETY: The `Project::project` impl preserves referent address.
+    unsafe impl<T: ?Sized> Cast<T, T> for IdCast {}
+
+    /// A pointer cast which preserves or shrinks the set of referent bytes of
+    /// a statically-sized referent.
+    ///
+    /// # Safety
+    ///
+    /// The implementation of [`Project`] uses a compile-time assertion to
+    /// guarantee that `Dst` is no larger than `Src`. Thus, `CastSized` has a
+    /// sound implementation of [`Project`] for all `Src` and `Dst` – the caller
+    /// may pass any `Src` and `Dst` without being responsible for soundness.
+    #[allow(missing_debug_implementations, missing_copy_implementations)]
+    pub enum CastSized {}
+
+    // SAFETY: By the `static_assert!`, `Dst` is no larger than `Src`,
+    // and `<*mut Src>::project` is a provenance-preserving cast.
+    unsafe impl<Src, Dst> Project<Src, Dst> for CastSized {
+        #[inline(always)]
+        unsafe fn project(src: *mut Src) -> *mut Dst {
+            static_assert!(Src, Dst => mem::size_of::<Src>() >= mem::size_of::<Dst>());
+            src.cast::<Dst>()
+        }
+    }
+
+    // SAFETY: The `Project::project` impl preserves referent address.
+    unsafe impl<Src, Dst> Cast<Src, Dst> for CastSized {}
+
+    /// A pointer cast which preserves or shrinks the set of referent bytes of
+    /// a dynamically-sized referent.
+    ///
+    /// # Safety
+    ///
+    /// The implementation of [`Project`] uses a compile-time assertion to
+    /// guarantee that the cast preserves the set of referent bytes. Thus,
+    /// `CastUnsized` has a sound implementation of [`Project`] for all `Src`
+    /// and `Dst` – the caller may pass any `Src` and `Dst` without being
+    /// responsible for soundness.
+    #[allow(missing_debug_implementations, missing_copy_implementations)]
+    pub enum CastUnsized {}
+
+    // SAFETY: The `static_assert!` ensures that `Src` and `Dst` have the same
+    // `SizeInfo`. Thus, casting preserves the set of referent bytes. All
+    // operations are provenance-preserving.
+    unsafe impl<Src, Dst> Project<Src, Dst> for CastUnsized
+    where
+        Src: ?Sized + KnownLayout,
+        Dst: ?Sized + KnownLayout<PointerMetadata = Src::PointerMetadata>,
+    {
+        #[inline(always)]
+        unsafe fn project(src: *mut Src) -> *mut Dst {
+            // FIXME:
+            // - Is the alignment check necessary for soundness? It's not
+            //   necessary for the soundness of the `Project` impl, but what
+            //   about the soundness of particular use sites?
+            // - Do we want this to support shrinking casts as well?
+            static_assert!(Src: ?Sized + KnownLayout, Dst: ?Sized + KnownLayout => {
+                let t = <Src as KnownLayout>::LAYOUT;
+                let u = <Dst as KnownLayout>::LAYOUT;
+                t.align.get() >= u.align.get() && match (t.size_info, u.size_info) {
+                    (SizeInfo::Sized { size: t }, SizeInfo::Sized { size: u }) => t == u,
+                    (
+                        SizeInfo::SliceDst(TrailingSliceLayout { offset: t_offset, elem_size: t_elem_size }),
+                        SizeInfo::SliceDst(TrailingSliceLayout { offset: u_offset, elem_size: u_elem_size })
+                    ) => t_offset == u_offset && t_elem_size == u_elem_size,
+                    _ => false,
+                }
+            });
+
+            let metadata = Src::pointer_to_metadata(src);
+            // SAFETY: The caller promises that `src` is non-null.
+            let src = unsafe { NonNull::new_unchecked(src) };
+            Dst::raw_from_ptr_len(src.cast::<u8>(), metadata).as_ptr()
+        }
+    }
+
+    // SAFETY: The `Project::project` impl preserves referent address.
+    unsafe impl<Src, Dst> Cast<Src, Dst> for CastUnsized
+    where
+        Src: ?Sized + KnownLayout,
+        Dst: ?Sized + KnownLayout<PointerMetadata = Src::PointerMetadata>,
+    {
+    }
+
+    /// A field projection
+    ///
+    /// A `Projection` is a [`Project`] which implements projection by
+    /// delegating to an implementation of [`HasField::project`].
+    #[allow(missing_debug_implementations, missing_copy_implementations)]
+    pub struct Projection<F: ?Sized, const VARIANT_ID: i128, const FIELD_ID: i128> {
+        _never: core::convert::Infallible,
+        _phantom: PhantomData<F>,
+    }
+
+    // SAFETY: `HasField::project` has the same safety post-conditions as
+    // `Project::project`.
+    unsafe impl<T: ?Sized, F, const VARIANT_ID: i128, const FIELD_ID: i128> Project<T, T::Type>
+        for Projection<F, VARIANT_ID, FIELD_ID>
+    where
+        T: HasField<F, VARIANT_ID, FIELD_ID>,
+    {
+        #[inline(always)]
+        unsafe fn project(src: *mut T) -> *mut T::Type {
+            // SAFETY: The caller promises that `src` is a non-null pointer
+            // whose referent is zero bytes or live in a single allocation.
+            unsafe { T::project(src) }
+        }
+    }
+
+    // SAFETY: All union fields exist at offset 0 within the union, and so any
+    // union projection is actually a cast (ie, preserves address).
+    //
+    // TODO: Is this actually true, even for repr(rust) unions?
+    unsafe impl<T: ?Sized, F, const FIELD_ID: i128> Cast<T, T::Type>
+        for Projection<F, { crate::UNION_VARIANT_ID }, FIELD_ID>
+    where
+        T: HasField<F, { crate::UNION_VARIANT_ID }, FIELD_ID>,
+    {
+    }
+
+    /// A transitive sequence of projections.
+    ///
+    /// Given `TU: Project` and `UV: Project`, `TransitiveProject<_, TU, UV>` is
+    /// a [`Project`] which projects by applying `TU` followed by `UV`.
+    ///
+    /// If `TU: Cast` and `UV: Cast`, then `TransitiveProject<_, TU, UV>: Cast`.
+    #[allow(missing_debug_implementations)]
+    pub struct TransitiveProject<U: ?Sized, TU, UV> {
+        _never: core::convert::Infallible,
+        _projections: PhantomData<(TU, UV)>,
+        // On our MSRV (1.56), the debuginfo for a tuple containing both an
+        // uninhabited type and a DST causes an ICE. We split `U` from `TU` and
+        // `UV` to avoid this situation.
+        _u: PhantomData<U>,
+    }
+
+    // SAFETY: Since `TU::project` and `UV::project` are each
+    // provenance-preserving operations which preserve or shrink the set of
+    // referent bytes, so is their composition.
+    unsafe impl<T, U, V, TU, UV> Project<T, V> for TransitiveProject<U, TU, UV>
+    where
+        T: ?Sized,
+        U: ?Sized,
+        V: ?Sized,
+        TU: Project<T, U>,
+        UV: Project<U, V>,
+    {
+        #[inline(always)]
+        unsafe fn project(t: *mut T) -> *mut V {
+            // SAFETY: The caller promises that `t` is a non-null pointer whose
+            // referent is zero-sized or lives in a single allocation.
+            let u = unsafe { TU::project(t) };
+            // SAFETY: By invariant on `TU::project`, `u` addresses a subset of
+            // the bytes of `*t`, and has provenance for all of those bytes.
+            // Thus, its referent is either zero-sized or lives in a single
+            // allocation. The first byte of the referent (its lowest address)
+            // lives in or one-past-the-end of `*t`. There are two cases:
+            // - `*t` is zero-sized, in which case `u` has the same address as
+            //   `t`, which is guaranteed by the caller to be non-null, or
+            // - `*t` lives in a single allocation, in which case none of its
+            //   bytes (nor the byte one past the end of it) live at the null
+            //   address [1].
+            //
+            // [1] https://doc.rust-lang.org/1.92.0/std/ptr/index.html#allocation
+            unsafe { UV::project(u) }
+        }
+    }
+
+    // SAFETY: Since the `Project::project` impl delegates to `TU::project` and
+    // `UV::project`, and since `TU` and `UV` are `Cast`, the `Project::project`
+    // impl preserves the address of the referent.
+    unsafe impl<T, U, V, TU, UV> Cast<T, V> for TransitiveProject<U, TU, UV>
+    where
+        T: ?Sized,
+        U: ?Sized,
+        V: ?Sized,
+        TU: Cast<T, U>,
+        UV: Cast<U, V>,
+    {
+    }
+
+    /// A cast from `T` to `[u8]`.
+    pub(crate) struct AsBytesCast;
+
+    // SAFETY: `project` constructs a pointer with the same address as `src`
+    // and with a referent of the same size as `*src`. It does this using
+    // provenance-preserving operations.
+    //
+    // FIXME(https://github.com/rust-lang/unsafe-code-guidelines/issues/594):
+    // Technically, this proof assumes that `*src` is contiguous (the same is
+    // true of other proofs in this codebase). Is this guaranteed anywhere?
+    unsafe impl<T: ?Sized + KnownLayout> Project<T, [u8]> for AsBytesCast {
+        #[inline(always)]
+        unsafe fn project(src: *mut T) -> *mut [u8] {
+            // SAFETY: The caller promises that `src` is non-null.
+            let src = unsafe { NonNull::new_unchecked(src) };
+            let bytes = match T::size_of_val_raw(src) {
+                Some(bytes) => bytes,
+                // SAFETY: `KnownLayout::size_of_val_raw` promises to always
+                // return `Some` so long as the resulting size fits in a
+                // `usize`. By invariant on `PtrInner`, `self` refers to a range
+                // of bytes whose size fits in an `isize`, which implies that it
+                // also fits in a `usize`.
+                None => unsafe { core::hint::unreachable_unchecked() },
+            };
+
+            core::ptr::slice_from_raw_parts_mut(src.cast::<u8>().as_ptr(), bytes)
+        }
+    }
+
+    // SAFETY: The `Project::project` impl preserves referent address.
+    unsafe impl<T: ?Sized + KnownLayout> Cast<T, [u8]> for AsBytesCast {}
+}
