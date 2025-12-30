6fe302c4229e8c688c8a4d1ae3951db74c6af419
Joshua Liebow-Feeser <joshlf@google.com>
2025-12-30 19:25:33 +0000

    [WIP]
    gherrit-pr-id: G60971ace7acef9cd1c74f74f397cb7c48e5e3f73

--- zerocopy-derive/src/lib.rs
+++ zerocopy-derive/src/lib.rs
@@ -870,20 +870,35 @@ fn derive_try_from_bytes_struct(
                 // validity of a struct is just the composition of the bit
                 // validities of its fields, so this is a sound implementation
                 // of `is_bit_valid`.
-                fn is_bit_valid<___ZerocopyAliasing>(
-                    mut candidate: #zerocopy_crate::Maybe<Self, ___ZerocopyAliasing>,
-                ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool
-                where
-                    ___ZerocopyAliasing: #zerocopy_crate::pointer::invariant::Reference,
-                {
+                fn is_bit_valid(
+                    mut candidate: #zerocopy_crate::Maybe<Self>,
+                ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool {
                     use #zerocopy_crate::util::macro_util::core_reexport;
                     use #zerocopy_crate::pointer::PtrInner;
+
+                    // SAFETY: `ReadOnly<Self>` has the same memory layout as `Self`.
+                    let mut candidate = unsafe {
+                        candidate.transmute_unchecked::<
+                            Self,
+                            _,
+                            #zerocopy_crate::pointer::cast::CastUnsized
+                        >()
+                    };
 
                     true #(&& {
                         let field_candidate = candidate.reborrow().project::<
                             _,
                             { #zerocopy_crate::ident_id!(#field_names) }
                         >();
+
+                        // SAFETY: `ReadOnly<T>` has the same memory layout as `T`.
+                        let field_candidate = unsafe {
+                             field_candidate.transmute_unchecked::<
+                                #zerocopy_crate::wrappers::ReadOnly<#field_tys>,
+                                _,
+                                #zerocopy_crate::pointer::cast::CastUnsized
+                             >()
+                        };
 
                         <#field_tys as #zerocopy_crate::TryFromBytes>::is_bit_valid(field_candidate)
                     })*
@@ -924,14 +939,20 @@ fn derive_try_from_bytes_union(
                 // The bit validity of a union is not yet well defined in Rust,
                 // but it is guaranteed to be no more strict than this
                 // definition. See #696 for a more in-depth discussion.
-                fn is_bit_valid<___ZerocopyAliasing>(
-                    mut candidate: #zerocopy_crate::Maybe<'_, Self,___ZerocopyAliasing>
-                ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool
-                where
-                    ___ZerocopyAliasing: #zerocopy_crate::pointer::invariant::Reference,
-                {
+                fn is_bit_valid(
+                    mut candidate: #zerocopy_crate::Maybe<'_, Self>
+                ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool {
                     use #zerocopy_crate::util::macro_util::core_reexport;
                     use #zerocopy_crate::pointer::PtrInner;
+
+                    // SAFETY: `ReadOnly<Self>` has the same memory layout as `Self`.
+                    let mut candidate = unsafe {
+                        candidate.transmute_unchecked::<
+                            Self,
+                            _,
+                            #zerocopy_crate::pointer::cast::CastUnsized
+                        >()
+                    };
 
                     false #(|| {
                         // SAFETY:
@@ -948,6 +969,15 @@ fn derive_try_from_bytes_union(
                                 _,
                                 #zerocopy_crate::pointer::cast::Projection<_, { #zerocopy_crate::UNION_VARIANT_ID }, { #zerocopy_crate::ident_id!(#field_names) }>
                             >()
+                        };
+
+                        // SAFETY: `ReadOnly<T>` has the same memory layout as `T`.
+                        let field_candidate = unsafe {
+                             field_candidate.transmute_unchecked::<
+                                #zerocopy_crate::wrappers::ReadOnly<#field_tys>,
+                                _,
+                                #zerocopy_crate::pointer::cast::CastUnsized
+                             >()
                         };
 
                         <#field_tys as #zerocopy_crate::TryFromBytes>::is_bit_valid(field_candidate)
@@ -1031,12 +1061,9 @@ fn try_gen_trivial_is_bit_valid(
     if matches!(top_level, Trait::FromBytes) && ast.generics.params.is_empty() {
         Some(quote!(
             // SAFETY: See inline.
-            fn is_bit_valid<___ZerocopyAliasing>(
-                _candidate: #zerocopy_crate::Maybe<Self, ___ZerocopyAliasing>,
-            ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool
-            where
-                ___ZerocopyAliasing: #zerocopy_crate::pointer::invariant::Reference,
-            {
+            fn is_bit_valid(
+                _candidate: #zerocopy_crate::Maybe<Self>,
+            ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool {
                 if false {
                     fn assert_is_from_bytes<T>()
                     where
@@ -1074,12 +1101,9 @@ unsafe fn gen_trivial_is_bit_valid_unchecked(zerocopy_crate: &Path) -> proc_macr
     quote!(
         // SAFETY: The caller of `gen_trivial_is_bit_valid_unchecked` has
         // promised that all initialized bit patterns are valid for `Self`.
-        fn is_bit_valid<___ZerocopyAliasing>(
-            _candidate: #zerocopy_crate::Maybe<Self, ___ZerocopyAliasing>,
-        ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool
-        where
-            ___ZerocopyAliasing: #zerocopy_crate::pointer::invariant::Reference,
-        {
+        fn is_bit_valid(
+            _candidate: #zerocopy_crate::Maybe<Self>,
+        ) -> #zerocopy_crate::util::macro_util::core_reexport::primitive::bool {
             true
         }
     )
