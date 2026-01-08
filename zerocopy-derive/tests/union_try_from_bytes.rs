e6bfe16d401ee98e43d954d3a50454a4932642e5
Joshua Liebow-Feeser <joshlf@google.com>
2026-01-08 18:53:44 +0000

    [WIP] Remove MutationCompatible, Read
    `MutationCompatible` introduces unnecessary indirection and makes many
    type signatures needlessly complex. In this commit, we flatten the trait
    hierarchy so that `TryTransmuteFromPtr` has more impls, and
    `MutationCompatible` is removed entirely.
    
    We also remove the `Read` trait for a similar reason.
    
    gherrit-pr-id: G8a3e5c48698ed092b1175cfb42f6f85fb3fb6a34

--- zerocopy-derive/tests/union_try_from_bytes.rs
+++ zerocopy-derive/tests/union_try_from_bytes.rs
@@ -125,7 +125,7 @@ util_assert_impl_all!(UnsafeCellUnion: imp::TryFromBytes);
 
 #[test]
 fn unsafe_cell_union() {
-    crate::util::test_is_bit_valid::<UnsafeCellUnion>([0u8], true);
-    crate::util::test_is_bit_valid::<UnsafeCellUnion>([1u8], true);
-    crate::util::test_is_bit_valid::<UnsafeCellUnion>([2u8], false);
+    crate::util::test_is_bit_valid::<UnsafeCellUnion, _>([0u8], true);
+    crate::util::test_is_bit_valid::<UnsafeCellUnion, _>([1u8], true);
+    crate::util::test_is_bit_valid::<UnsafeCellUnion, _>([2u8], false);
 }
