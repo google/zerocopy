797a0ab6340bb544d598b224490e3c35a84c967b
Joshua Liebow-Feeser <joshlf@google.com>
2026-01-12 16:49:55 +0000

    Vendor dependencies
    This allows us to simplify the logic which ensures that we never depend
    on crate versions which are incompatible with our MSRV. It also avoids a
    performance bottleneck on our MSRV in which updating the crates.io index
    is very slow.
    
    gherrit-pr-id: Gd8dc83951469fd1467ddb65d2ac524b709fe9503

---
+++ vendor/time/src/error/try_from_parsed.rs
@@ -0,0 +1,52 @@
+//! Error converting a [`Parsed`](crate::parsing::Parsed) struct to another type
+
+use core::fmt;
+
+use crate::error;
+
+/// An error that occurred when converting a [`Parsed`](crate::parsing::Parsed) to another type.
+#[non_exhaustive]
+#[cfg_attr(__time_03_docs, doc(cfg(feature = "parsing")))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TryFromParsed {
+    /// The [`Parsed`](crate::parsing::Parsed) did not include enough information to construct the
+    /// type.
+    InsufficientInformation,
+    /// Some component contained an invalid value for the type.
+    ComponentRange(error::ComponentRange),
+}
+
+impl fmt::Display for TryFromParsed {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InsufficientInformation => f.write_str(
+                "the `Parsed` struct did not include enough information to construct the type",
+            ),
+            Self::ComponentRange(err) => err.fmt(f),
+        }
+    }
+}
+
+impl From<error::ComponentRange> for TryFromParsed {
+    fn from(v: error::ComponentRange) -> Self {
+        Self::ComponentRange(v)
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(__time_03_docs, doc(cfg(feature = "std")))]
+impl std::error::Error for TryFromParsed {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::InsufficientInformation => None,
+            Self::ComponentRange(err) => Some(err),
+        }
+    }
+}
+
+#[cfg_attr(__time_03_docs, doc(cfg(feature = "parsing")))]
+impl From<TryFromParsed> for crate::Error {
+    fn from(original: TryFromParsed) -> Self {
+        Self::TryFromParsed(original)
+    }
+}
---
+++ vendor/trybuild/src/error.rs
@@ -0,0 +1,95 @@
+use glob::{GlobError, PatternError};
+use std::ffi::OsString;
+use std::fmt::{self, Display};
+use std::io;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub enum Error {
+    Cargo(io::Error),
+    CargoFail,
+    GetManifest(PathBuf, Box<Error>),
+    Glob(GlobError),
+    Io(io::Error),
+    Metadata(serde_json::Error),
+    Mismatch,
+    NoWorkspaceManifest,
+    Open(PathBuf, io::Error),
+    Pattern(PatternError),
+    ProjectDir,
+    ReadStderr(io::Error),
+    RunFailed,
+    ShouldNotHaveCompiled,
+    Toml(basic_toml::Error),
+    UpdateVar(OsString),
+    WriteStderr(io::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Error::*;
+
+        match self {
+            Cargo(e) => write!(f, "failed to execute cargo: {}", e),
+            CargoFail => write!(f, "cargo reported an error"),
+            GetManifest(path, e) => write!(f, "failed to read manifest {}: {}", path.display(), e),
+            Glob(e) => write!(f, "{}", e),
+            Io(e) => write!(f, "{}", e),
+            Metadata(e) => write!(f, "failed to read cargo metadata: {}", e),
+            Mismatch => write!(f, "compiler error does not match expected error"),
+            NoWorkspaceManifest => write!(f, "Cargo.toml uses edition.workspace=true, but no edition found in workspace's manifest"),
+            Open(path, e) => write!(f, "{}: {}", path.display(), e),
+            Pattern(e) => write!(f, "{}", e),
+            ProjectDir => write!(f, "failed to determine name of project dir"),
+            ReadStderr(e) => write!(f, "failed to read stderr file: {}", e),
+            RunFailed => write!(f, "execution of the test case was unsuccessful"),
+            ShouldNotHaveCompiled => {
+                write!(f, "expected test case to fail to compile, but it succeeded")
+            }
+            Toml(e) => write!(f, "{}", e),
+            UpdateVar(var) => write!(
+                f,
+                "unrecognized value of TRYBUILD: {:?}",
+                var.to_string_lossy(),
+            ),
+            WriteStderr(e) => write!(f, "failed to write stderr file: {}", e),
+        }
+    }
+}
+
+impl Error {
+    pub fn already_printed(&self) -> bool {
+        use self::Error::*;
+
+        matches!(
+            self,
+            CargoFail | Mismatch | RunFailed | ShouldNotHaveCompiled
+        )
+    }
+}
+
+impl From<GlobError> for Error {
+    fn from(err: GlobError) -> Self {
+        Error::Glob(err)
+    }
+}
+
+impl From<PatternError> for Error {
+    fn from(err: PatternError) -> Self {
+        Error::Pattern(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
+impl From<basic_toml::Error> for Error {
+    fn from(err: basic_toml::Error) -> Self {
+        Error::Toml(err)
+    }
+}
