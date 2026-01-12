16b4ff3d2f14d046bf3f7c0adead44ad12f18947
Joshua Liebow-Feeser <joshlf@google.com>
2026-01-12 15:43:54 +0000

    [derive] Unify handling of #[allow] attributes
    Introduce a single `const_block` utility function which generates a
    `const _: () = { ... };` block annotated with all relevant
    `#[allow(...)]` attributes. Remove `#[allow(...)]` attributes from most
    other places in the codebase.
    
    Add `#[allow(non_ascii_idents)]` to this list.
    
    Closes #2880
    
    gherrit-pr-id: G51354cfb7f9717a2e63a11c83c74dacce29366f2

---
+++ zerocopy-derive/tests/issue_2880.rs
@@ -0,0 +1,34 @@
+// Copyright 2025 The Fuchsia Authors
+//
+// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
+// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
+
+#![no_implicit_prelude]
+#![deny(non_ascii_idents)]
+
+include!("include.rs");
+
+// NOTE: We don't derive `FromBytes` because that would result in a trivial
+// `is_bit_valid` impl, which would fail to test most or all of the problematic
+// generated code.
+
+#[derive(imp::KnownLayout, imp::IntoBytes, imp::Immutable)]
+#[repr(C)]
+struct TestStruct {
+    a: u8,
+}
+
+#[derive(imp::KnownLayout, imp::IntoBytes, imp::Immutable)]
+#[repr(u8)]
+enum TestEnum {
+    A = 0,
+}
+
+#[derive(imp::KnownLayout, imp::IntoBytes, imp::Immutable)]
+#[repr(C)]
+union TestUnion {
+    a: u8,
+}
