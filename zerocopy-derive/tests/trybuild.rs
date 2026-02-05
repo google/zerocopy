e51d843d7e0bda437b4c342e57d135d2bc1d534b
Joshua Liebow-Feeser <joshlf@google.com>
2026-02-05 14:29:53 +0000

    [tests] Rely on `cargo-zerocopy` toolchain resolution
    `cargo-zerocopy` now passes the toolchain name in `--cfg
    __ZEROCOPY_INTERNAL_USE_ONLY_TOOLCHAIN="..."`. In the trybuild test
    machinery in `testutil`, use this instead of attempting to dynamically
    re-discover the toolchain name.
    
    gherrit-pr-id: G84649ff5c69192524bd50158850a16250dbd194c

--- zerocopy-derive/tests/trybuild.rs
+++ zerocopy-derive/tests/trybuild.rs
@@ -10,13 +10,14 @@
 
 use std::env;
 
-use testutil::set_rustflags_w_warnings;
+use testutil::{set_rustflags_w_warnings, ToolchainVersion};
 
 fn test(subdir: &str) {
-    let version = testutil::ToolchainVersion::extract_from_pwd().unwrap();
     // See the doc comment on this method for an explanation of what this does
     // and why we store source files in different directories.
-    let source_files_dirname = version.get_ui_source_files_dirname_and_maybe_print_warning();
+    let source_files_dirname = ToolchainVersion::extract_from_env()
+        .expect("UI tests must only be run on pinned MSRV, stable, or nightly toolchains")
+        .get_ui_source_files_dirname();
 
     // Set `-Wwarnings` in the `RUSTFLAGS` environment variable to ensure that
     // `.stderr` files reflect what the typical user would encounter.
