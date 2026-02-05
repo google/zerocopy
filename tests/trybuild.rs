4f98f5c99ad7be087496925b3b1a9dad0c9c8158
Joshua Liebow-Feeser <joshlf@users.noreply.github.com>
2026-02-05 14:15:38 +0000

    [test] Move `impl_or_verify!` tests to `doctests` (#2988)
    They require a lot of fragile machinery as UI tests, and are much
    simpler as doc tests.
    
    gherrit-pr-id: G793f61a136b912fbd5ee4315cdea7cf3cf3cf8fb

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -37,19 +37,3 @@ fn ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail(format!("tests/{}/*.rs", source_files_dirname));
 }
-
-#[test]
-#[cfg_attr(miri, ignore)]
-fn ui_invalid_impls() {
-    let version = ToolchainVersion::extract_from_pwd().unwrap();
-    // See the doc comment on this method for an explanation of what this does
-    // and why we store source files in different directories.
-    let source_files_dirname = version.get_ui_source_files_dirname_and_maybe_print_warning();
-
-    // Set `-Wwarnings` in the `RUSTFLAGS` environment variable to ensure that
-    // `.stderr` files reflect what the typical user would encounter.
-    set_rustflags_w_warnings();
-
-    let t = trybuild::TestCases::new();
-    t.compile_fail(format!("tests/{}/invalid-impls/*.rs", source_files_dirname));
-}
