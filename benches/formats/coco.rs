48b088242197bb9d5509986bd7072d846bba664c
Jack Wrenn <jswrenn@google.com>
2026-02-12 20:44:22 +0000

    [WIP] Codegen tests
    gherrit-pr-id: G5964d13c9ffb7a47ed4662892ef92c90ddc51e12

---
+++ benches/formats/coco.rs
@@ -0,0 +1,22 @@
+use zerocopy_derive::*;
+
+// The only valid value of this type is the byte `0xC0`
+#[derive(TryFromBytes, KnownLayout, Immutable)]
+#[repr(u8)]
+enum C0 {
+    _XC0 = 0xC0,
+}
+
+// The only valid value of this type is the bytes `0xC0C0`.
+#[derive(TryFromBytes, KnownLayout, Immutable)]
+#[repr(C)]
+struct C0C0(C0, C0);
+
+#[derive(TryFromBytes, KnownLayout, Immutable)]
+#[repr(C)]
+pub struct Packet {
+    magic_number: C0C0,
+    mug_size: u8,
+    temperature: u8,
+    marshmallows: [[u8; 2]],
+}
