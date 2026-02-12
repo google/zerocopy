d15d51feb044295ee7f745639a6fa5fafed57841
Jack Wrenn <jswrenn@google.com>
2026-02-12 20:44:22 +0000

    Add code generation tests
    Introduces a suite of code generation tests to the `benches` folder.
    Each tests consists of a pair of files: a Rust source containing a
    function named `codegen_test`, and a `.x86-64.mca` file containing
    the llvm-mca analysis of `codegen_test`. In practice, these tests
    operate similarly to UI tests; CI checks a fresh llvm-mca analysis
    against that in the file, and fails if they mismatch. Changes can
    be expressly blessed via `BLESS=1`.
    
    gherrit-pr-id: G5964d13c9ffb7a47ed4662892ef92c90ddc51e12

---
+++ benches/formats/coco.rs
@@ -0,0 +1,25 @@
+// Copyright 2026 The Fuchsia Authors
+//
+// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
+// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
+
+use zerocopy_derive::*;
+
+// The only valid value of this type are the bytes `0xC0C0`.
+#[derive(TryFromBytes, KnownLayout, Immutable)]
+#[repr(u16, align(2))]
+enum C0C0 {
+    _XC0C0 = 0xC0C0,
+}
+
+#[derive(TryFromBytes, KnownLayout, Immutable)]
+#[repr(C)]
+pub struct Packet {
+    magic_number: C0C0,
+    mug_size: u8,
+    temperature: u8,
+    marshmallows: [[u8; 2]],
+}
