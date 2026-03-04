af96890643a82d0b78cf1262cdd306c0c5c1599f
Jack Wrenn <jswrenn@google.com>
2026-03-04 17:32:38 +0000

    Introduce 'Code Generation' documentation
    Threads each of the codegen benchmarks introduced in #3042 into the
    rustdoc of the corresponding item.
    
    gherrit-pr-id: G490f3d0c05cc9f6db9c019ac73da4215f51dc627

--- benches/try_transmute_ref.rs
+++ benches/try_transmute_ref.rs
@@ -1,12 +1,3 @@
-// Copyright 2026 The Fuchsia Authors
-//
-// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
-// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
-// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
-// This file may not be copied, modified, or distributed except according to
-// those terms.
-
-use zerocopy::try_transmute_ref;
 use zerocopy_derive::*;
 
 #[path = "formats/coco.rs"]
@@ -21,5 +12,5 @@ struct MinimalViableSource {
 
 #[unsafe(no_mangle)]
 fn codegen_test(source: &MinimalViableSource) -> Option<&format::CocoPacket> {
-    try_transmute_ref!(source).ok()
+    zerocopy::try_transmute_ref!(source).ok()
 }
