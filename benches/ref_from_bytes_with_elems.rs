8b4c98b35ccf7994ddd3cb4d06de81064b4d8422
Jack Wrenn <jswrenn@google.com>
2026-03-04 17:32:38 +0000

    Introduce 'Code Generation' documentation
    Threads each of the codegen benchmarks introduced in #3042 into the
    rustdoc of the corresponding item.
    
    gherrit-pr-id: G490f3d0c05cc9f6db9c019ac73da4215f51dc627

--- benches/ref_from_bytes_with_elems.rs
+++ benches/ref_from_bytes_with_elems.rs
@@ -1,17 +1,7 @@
-// Copyright 2026 The Fuchsia Authors
-//
-// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
-// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
-// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
-// This file may not be copied, modified, or distributed except according to
-// those terms.
-
-use zerocopy::FromBytes;
-
 #[path = "formats/coco.rs"]
 mod format;
 
 #[unsafe(no_mangle)]
 fn codegen_test(source: &[u8], count: usize) -> Option<&format::LocoPacket> {
-    FromBytes::ref_from_bytes_with_elems(source, count).ok()
+    zerocopy::FromBytes::ref_from_bytes_with_elems(source, count).ok()
 }
