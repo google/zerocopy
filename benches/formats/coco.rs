bd8bd7673227bdc77e4c920740d469378229c760
Jack Wrenn <jswrenn@google.com>
2026-03-04 17:32:38 +0000

    Introduce 'Code Generation' documentation
    Threads each of the codegen benchmarks introduced in #3042 into the
    rustdoc of the corresponding item.
    
    gherrit-pr-id: G490f3d0c05cc9f6db9c019ac73da4215f51dc627

--- benches/formats/coco.rs
+++ benches/formats/coco.rs
@@ -1,24 +1,3 @@
-// Copyright 2026 The Fuchsia Authors
-//
-// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
-// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
-// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
-// This file may not be copied, modified, or distributed except according to
-// those terms.
-
-//! This modules defines two formats similar to the one used to illustrate the
-//! examples of [`zerocopy::TryFromBytes`]. The basic structure of these formats
-//! is a [`Packet`], which has a parameterizable leading field, followed by
-//! two-fixed byte fields, followed by a dynamically sized field. This format is
-//! consumed by our benchmarks either as a [`CocoPacket`] (which begins with the
-//! leading bytes `0xC0C0`), or a [`LocoPacket`] (which begins two bytes of any
-//! value). Both formats share the following qualities which stress-test our
-//! benchmarks:
-//!
-//! - They have a minimum alignment of two.
-//! - They have a minimum size of four bytes.
-//! - They have an even size.
-
 use zerocopy_derive::*;
 
 // The only valid value of this type are the bytes `0xC0C0`.
