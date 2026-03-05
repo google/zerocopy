bdb14803d7c380cef17280b0b045810e313356d9
Jack Wrenn <jswrenn@google.com>
2026-03-05 15:36:09 +0000

    [codegen] Track unanalyzed assembly and include it in docs.
    This is more approachable than the full llvm-mca analysis.
    
    gherrit-pr-id: Ged67b8f5ad7d38d9f2eb40adebea8894a1a6a008

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -10,52 +10,83 @@
 
 use std::{path::PathBuf, process::Command};
 
+enum Directive {
+    Asm,
+    Mca,
+}
+
+impl Directive {
+    fn arg(&self) -> &'static str {
+        match self {
+            Directive::Asm => "--asm",
+            Directive::Mca => "--mca",
+        }
+    }
+
+    fn ext(&self) -> &'static str {
+        match self {
+            Directive::Asm => "",
+            Directive::Mca => ".mca",
+        }
+    }
+}
+
 fn run_codegen_test(bench_name: &str, target_cpu: &str, bless: bool) {
     let manifest_path = env!("CARGO_MANIFEST_PATH");
     let target_dir = env!("CARGO_TARGET_DIR");
 
-    let output = Command::new("cargo")
-        .args([
-            "asm",
-            "-p",
-            "zerocopy",
-            "--manifest-path",
-            manifest_path,
-            "--target-dir",
-            target_dir,
-            "--bench",
-            bench_name,
-            "--target-cpu",
-            target_cpu,
-            "--mca",
-            "codegen_test",
-        ])
-        .output()
-        .expect("failed to execute process");
-
-    let actual_result = output.stdout;
-
-    if !(output.status.success()) {
-        panic!("{}", String::from_utf8_lossy(&output.stderr));
-    }
-
-    let expected_file_path = {
-        let mut path: PathBuf = env!("CARGO_MANIFEST_DIR").into();
-        path.push("benches");
-        let file_name = format!("{bench_name}.{target_cpu}.mca");
-        path.push(file_name);
-        path
+    let cargo_asm = |directive: &Directive| {
+        Command::new("cargo")
+            .args([
+                "asm",
+                "-p",
+                "zerocopy",
+                "--manifest-path",
+                manifest_path,
+                "--target-dir",
+                target_dir,
+                "--bench",
+                bench_name,
+                "--target-cpu",
+                target_cpu,
+                "--simplify",
+                directive.arg(),
+                "codegen_test",
+            ])
+            .output()
+            .expect("failed to execute process")
     };
 
-    if bless {
-        std::fs::write(expected_file_path, &actual_result).unwrap();
-    } else {
-        let expected_result = std::fs::read(expected_file_path).unwrap_or_default();
-        if actual_result != expected_result {
-            let expected = String::from_utf8_lossy(&expected_result[..]);
-            panic!("Bless codegen tests with BLESS=1\nGot unexpected output:\n{}", expected);
+    let test_directive = |directive: Directive| {
+        let output = cargo_asm(&directive);
+        let actual_result = output.stdout;
+
+        if !(output.status.success()) {
+            panic!("{}", String::from_utf8_lossy(&output.stderr));
         }
-    }
+
+        let expected_file_path = {
+            let ext = directive.ext();
+            let mut path: PathBuf = env!("CARGO_MANIFEST_DIR").into();
+            path.push("benches");
+            let file_name = format!("{bench_name}.{target_cpu}{ext}",);
+            path.push(file_name);
+            path
+        };
+
+        if bless {
+            std::fs::write(expected_file_path, &actual_result).unwrap();
+        } else {
+            let expected_result = std::fs::read(expected_file_path).unwrap_or_default();
+            if actual_result != expected_result {
+                let expected = String::from_utf8_lossy(&expected_result[..]);
+                panic!("Bless codegen tests with BLESS=1\nGot unexpected output:\n{}", expected);
+            }
+        }
+    };
+
+    test_directive(Directive::Asm);
+    test_directive(Directive::Mca);
 }
 
 #[test]
