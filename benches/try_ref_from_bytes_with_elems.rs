f5a1f81a599d5f878796a7b0c6a26ca35eb48f6c
Jack Wrenn <jswrenn@google.com>
2026-03-06 20:34:21 +0000

    [codegen] Test and document codegen under different layouts
    This improves the robustness of our suite to different code paths, and
    illustrates in our documentation the relationship between type
    complexity and codegen complexity.
    
    gherrit-pr-id: G39e297d5b891f5115ef9607b090c5fbe95427d68

--- benches/try_ref_from_bytes_with_elems.rs
+++
@@ -1,7 +0,0 @@
-#[path = "formats/coco.rs"]
-mod format;
-
-#[unsafe(no_mangle)]
-fn bench_try_ref_from_bytes_with_elems(source: &[u8], count: usize) -> Option<&format::CocoPacket> {
-    zerocopy::TryFromBytes::try_ref_from_bytes_with_elems(source, count).ok()
-}
