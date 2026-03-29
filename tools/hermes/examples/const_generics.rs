859e3fe01ca079c990156442dc750284b36c2e34
Joshua Liebow-Feeser <joshlf@google.com>
2026-03-29 21:08:47 +0000

    [hermes] Fix more integration tests
    gherrit-pr-id: Gp3vcrzld5qqjbxn7ihbj7l724vie5ste

--- tools/hermes/examples/const_generics.rs
+++ tools/hermes/examples/const_generics.rs
@@ -1,4 +1,15 @@
-/// ```lean, hermes, spec
+/// ```hermes
+/// isValid self := N > 0
+/// ```
+pub struct ConstGen<const N: usize>;
+
+/// ```hermes
+/// isSafe : N > 0
+/// ```
+pub unsafe trait ConstTrait<const N: usize> {}
+
+/// ```hermes
+/// ensures: if N.val = 0 then ret.val = 0 else True
 /// ```
 pub fn use_const<const N: usize>(arr: [u8; N]) -> u8 {
     if N > 0 { arr[0] } else { 0 }
