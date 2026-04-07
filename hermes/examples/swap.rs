36e18222d0c34481135ba53efac85b91ee283a9d
Joshua Liebow-Feeser <joshlf@google.com>
2026-04-07 02:57:16 +0000

    [hermes] Unfold, use `grind` in automatic proofs
    In `autoParam` automatic proofs, insert `unfold` and `grind` tactics in
    various places. `unfold` is crucial – without it, *only* statements
    which are universally true for all possible implementations of the
    function signature can be automatically proven. `grind` is an SMT-like
    tactic which is able to automatically discharge a wide range of proofs.
    
    Closes #3210
    Closes #3213
    
    gherrit-pr-id: G5iiniran72qxcjixnuxghrjo5paoe57n

--- hermes/examples/swap.rs
+++ hermes/examples/swap.rs
@@ -3,12 +3,6 @@
 /// ```lean, hermes, spec
 /// ensures(h_x'_eq_y): x' = y
 /// ensures(h_y'_eq_x): y' = x
-/// proof(h_x'_eq_y):
-///   unfold swap at h_returns
-///   simp_all
-/// proof(h_y'_eq_x):
-///   unfold swap at h_returns
-///   simp_all
 /// proof (h_progress):
 ///   unfold swap
 ///   simp_all
