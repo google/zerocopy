3d667acc1da2bd3ee292a062558b1b994a0584e3
github-action-benchmark <github@users.noreply.github.com>
2026-04-16 14:46:51 +0000

    add CI Durations (customSmallerIsBetter) benchmark result for 15639839eafdc9f22448b1e2d6c30ff45f80f080

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776349782826,
+  "lastUpdate": 1776350811100,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -421,6 +421,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Test Time",
             "value": 582,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Joshua Liebow-Feeser",
+            "username": "joshlf",
+            "email": "joshlf@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "15639839eafdc9f22448b1e2d6c30ff45f80f080",
+          "message": "[ci][anneal] Add precompiled artifact build script and workflow (#3281)\n\nTo automate the creation of precompiled artifacts for the Anneal\ntoolchain, we add a build script and integrate it into the release\nworkflow. This will allow us to simplify the `setup` command to simply\ndownload these pre-built artifacts from a single location, avoid needing\nto build from source on the user's machine, and download fewer artifacts\n(in particular, stripping out Mathlib modules which are unused by\nAnneal). This is especially important for development *on* Anneal and\nfor CI, which run the `setup` command frequently.\n\ngherrit-pr-id: Gigvceuv7utvaq4hymnx3dl22qewo6vuz",
+          "timestamp": "2026-04-16T14:28:47Z",
+          "url": "https://github.com/google/zerocopy/commit/15639839eafdc9f22448b1e2d6c30ff45f80f080"
+        },
+        "date": 1776350809248,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Pull Time",
+            "value": 67,
+            "unit": "seconds"
+          },
+          {
+            "name": "Test Time",
+            "value": 777,
             "unit": "seconds"
           }
         ]
