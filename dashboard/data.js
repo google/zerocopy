a6ef448e1982b26b1b5fd0066b6170db00ee4eb8
github-action-benchmark <github@users.noreply.github.com>
2026-04-20 11:03:13 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for 46a430c37c828efce073211d3c64c0000e84f5ef

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776676347154,
+  "lastUpdate": 1776682993483,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -1811,6 +1811,31 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/google/zerocopy/commit/3a99d7582663d8082df5b23ed4fd793b4e124211"
         },
         "date": 1776675558736,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 7,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "46a430c37c828efce073211d3c64c0000e84f5ef",
+          "message": "[CI] Bump the all-actions group with 8 updates",
+          "timestamp": "2026-04-20T08:58:54Z",
+          "url": "https://github.com/google/zerocopy/pull/3301/commits/46a430c37c828efce073211d3c64c0000e84f5ef"
+        },
+        "date": 1776682991584,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
