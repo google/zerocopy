436cae7436c6cba9ef03223a4a19bf6cdc5d990d
github-action-benchmark <github@users.noreply.github.com>
2026-04-22 10:58:23 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for fa91316f61deeda4fe2d77de7db494fc1f3103b5

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776854976242,
+  "lastUpdate": 1776855503074,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -2746,6 +2746,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "Docker Build Time",
             "value": 11,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "fa91316f61deeda4fe2d77de7db494fc1f3103b5",
+          "message": "Make Docker image tag and cache volume unique per worktree",
+          "timestamp": "2026-04-21T21:53:26Z",
+          "url": "https://github.com/google/zerocopy/pull/3322/commits/fa91316f61deeda4fe2d77de7db494fc1f3103b5"
+        },
+        "date": 1776855500494,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 9,
             "unit": "seconds"
           }
         ]
